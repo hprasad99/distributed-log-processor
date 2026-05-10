e0e5fa10fd371051ed119e056af58ba84dc63dbc
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-05-10 19:46:43 +0530

    SIERRA-001: fix multi-module gradle build and docker-compose infrastructure setup.

---
+++ log-consumer/src/main/java/org/project/logprocessor/consumer/model/LogEvent.java
@@ -0,0 +1,40 @@
+package org.project.logprocessor.consumer.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "log_events")
+@Getter
+@Setter
+public class LogEvent {
+
+    @Id
+    private String id;
+
+    @Column(name="organization_id", nullable = false)
+    private String organizationId;
+
+    @Column(name = "level", nullable = false)
+    private String level;
+
+    @Column(name = "message", columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "source")
+    private String source;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "processed_at")
+    private  LocalDateTime processadAt;
+
+    public LogEvent() {}
+}
---
+++ log-producer/src/main/java/org/project/logprocessor/producer/enums/LogLevel.java
@@ -0,0 +1,7 @@
+package org.project.logprocessor.producer.enums;
+
+public enum LogLevel {
+    INFO,
+    WARN,
+    ERROR;
+}
