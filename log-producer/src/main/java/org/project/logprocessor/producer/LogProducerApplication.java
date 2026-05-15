12fa58ef810f72dd8099cba93862161d059d75a3
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-05-16 02:08:07 +0530

    SIERRA-006: fixed application configuration

--- log-producer/src/main/java/org/project/logprocessor/producer/LogProducerApplication.java
+++ log-producer/src/main/java/org/project/logprocessor/producer/LogProducerApplication.java
@@ -2,8 +2,10 @@ package org.project.logprocessor.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class LogProducerApplication {
   public static void main(String[] args) {
     SpringApplication.run(LogProducerApplication.class, args);
