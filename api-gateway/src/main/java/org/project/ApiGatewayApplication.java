81608df662e800aac42e0320d7640d2ba4d3e639
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-05-16 22:52:54 +0530

    SIERRA-008: fixed api-gateway configuration

--- api-gateway/src/main/java/org/project/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/project/ApiGatewayApplication.java
@@ -1,10 +1,13 @@
 package org.project;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApiGatewayApplication {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    SpringApplication.run(ApiGatewayApplication.class, args);
   }
 }
