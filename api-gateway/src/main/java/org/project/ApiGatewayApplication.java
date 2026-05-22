5cd9d3a496a54b8cd283eda80ab85375fa406c54
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-05-22 16:19:42 +0530

    SIERRA-010: fixed dependency

--- api-gateway/src/main/java/org/project/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/project/ApiGatewayApplication.java
@@ -2,11 +2,8 @@ package org.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class ApiGatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(ApiGatewayApplication.class, args);
--- log-producer/src/main/java/org/project/logprocessor/producer/LogProducerApplication.java
+++ log-producer/src/main/java/org/project/logprocessor/producer/LogProducerApplication.java
@@ -2,11 +2,8 @@ package org.project.logprocessor.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class LogProducerApplication {
   public static void main(String[] args) {
     SpringApplication.run(LogProducerApplication.class, args);
