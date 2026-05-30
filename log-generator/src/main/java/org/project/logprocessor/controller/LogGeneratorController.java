5647a9d862f8b61cd16d072211f97e1609e26148
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-05-30 22:52:03 +0530

    [WIP-015]: added LogGenerationService

--- log-generator/src/main/java/org/project/logprocessor/controller/LogGeneratorController.java
+++ log-generator/src/main/java/org/project/logprocessor/controller/LogGeneratorController.java
@@ -1,6 +1,11 @@
 package org.project.logprocessor.controller;
 
+import java.util.Map;
+
 import org.project.logprocessor.service.LogGenerationService;
+import org.project.logprocessor.service.RateLimitingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/generator")
 public class LogGeneratorController {
   private final LogGenerationService logGenerationService;
+  private final RateLimitingService rateLimitingService;
 
-  public LogGeneratorController(LogGenerationService logGenerationService) {
+  public LogGeneratorController(
+      LogGenerationService logGenerationService, RateLimitingService rateLimitingService) {
     this.logGenerationService = logGenerationService;
+    this.rateLimitingService = rateLimitingService;
+  }
+
+  @PostMapping("/start")
+  public ResponseEntity<Map<String, String>> startGeneration() {
+    // logGenerationService.start
+    return null;
   }
 }
--- log-generator/src/main/java/org/project/logprocessor/service/LogGenerationService.java
+++ log-generator/src/main/java/org/project/logprocessor/service/LogGenerationService.java
@@ -1,5 +1,10 @@
 package org.project.logprocessor.service;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,8 +15,20 @@ public class LogGenerationService {
   private static final Logger logger = LoggerFactory.getLogger(LogGenerationService.class);
 
   private final KafkaProducerService kafkaProducerService;
+  private final RateLimitingService rateLimitingService;
+  private final ExecutorService executorService;
+  private final Random random = new Random();
 
-  public LogGenerationService(KafkaProducerService kafkaProducerService) {
+  private final AtomicLong generatedCounter = new AtomicLong(0);
+  private final AtomicLong rateLimitedCounter = new AtomicLong(0);
+  private final AtomicInteger currentRatePerSecond = new AtomicInteger(0);
+
+  public LogGenerationService(
+      KafkaProducerService kafkaProducerService,
+      RateLimitingService rateLimitingService,
+      ExecutorService executorService) {
     this.kafkaProducerService = kafkaProducerService;
+    this.rateLimitingService = rateLimitingService;
+    this.executorService = executorService;
   }
 }
