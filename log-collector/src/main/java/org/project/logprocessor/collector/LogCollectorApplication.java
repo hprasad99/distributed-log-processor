245a0d320e7e0db1d9558c59a5b6915341bc1665
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-06-21 12:24:57 +0530

    [WIP-016]: added main collector application

--- log-collector/src/main/java/org/project/logprocessor/collector/LogCollectorApplication.java
+++ log-collector/src/main/java/org/project/logprocessor/collector/LogCollectorApplication.java
@@ -1,3 +1,15 @@
 package org.project.logprocessor.collector;
 
-public class LogCollectorApplication {}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class LogCollectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogCollectorApplication.class, args);
+    }
+}
