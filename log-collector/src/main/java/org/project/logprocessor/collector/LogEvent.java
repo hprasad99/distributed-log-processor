a6a59c763aeaeb187f06abee03fba019367e2a45
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-06-21 12:22:57 +0530

    [WIP-016]: added implementation for producer service and logevent model.

--- log-collector/src/main/java/org/project/logprocessor/collector/LogEvent.java
+++ log-collector/src/main/java/org/project/logprocessor/collector/LogEvent.java
@@ -1,3 +1,30 @@
 package org.project.logprocessor.collector;
 
-public class LogEvent {}
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LogEvent {
+  private String id;
+  private String filePath;
+  private String content;
+  private long fileOffset;
+  private LocalDateTime timestamp;
+  private String hostname;
+  private String serviceName;
+  private String contentHash;
+
+  private String generateId() {
+    return String.format("%s-%d-%s", filePath.hashCode(), fileOffset, timestamp.toString());
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "LogEvent{id='%s', filePath='%s', content='%s', offset=%d}",
+        id, filePath, content.substring(0, Math.min(50, content.length())), fileOffset);
+  }
+}
